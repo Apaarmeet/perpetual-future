73e25aa0f890bb83bbfe9257c8a51b1d3c099b80
Apaarmeet <apaarmeet5000@gmail.com>
2026-06-08 18:36:59 +0530

    made the balance and 2 position endpoints

--- apps/server/src/routes/engine.routes.ts
+++ apps/server/src/routes/engine.routes.ts
@@ -82,7 +82,7 @@ engineRouter.post("/onramp",async (req, res) => {
        {
            correlationId,
            responseTo: `response:${backendId}`,
-           type:"onRamp",
+           requestType:"onRamp",
            userId: user.id,
            symbol: "USD",
            amount: data.amount.toString()
@@ -124,6 +124,7 @@ engineRouter.post("/order", async (req, res) => {
         {
             correlationId,
             responseTo: `response:${backendId}`,
+            requestType: "create-order",
             userId: user.id,
             type: data.type,
             side: data.side,
@@ -169,6 +170,7 @@ engineRouter.delete("/order", async (req, res) => {
     {
         correlationId,
         responseTo: `response:${backendId}`,
+        requestType: "cancel-order",
         userId: user.id,
         orderId: data.orderId,
     }
@@ -180,8 +182,84 @@ engineRouter.delete("/order", async (req, res) => {
     response
    })
 })
-engineRouter.get("/equity/available", (req, res) => {})
-engineRouter.get("/positions/open/:marketId", (req, res) => {});
+engineRouter.get("/equity/available", async (req, res) => {
+   const user = req.user;
+  const correlationId = crypto.randomUUID();
+
+  const engineRequest = new Promise((resolve) => {
+    pendingRequest.set(correlationId, resolve);
+  });
+
+  await client.xAdd("engine:request", "*", {
+    correlationId,
+    responseTo: `response:${backendId}`,
+    requestType: "get-user-balance",
+    userId: user.id,
+  });
+
+  const response = await engineRequest;
+  return res.json({ response });  
+})
+engineRouter.get("/positions/open/:marketId", async (req, res) => {
+   const symbol = req.params.marketId;
+   const user = req.user;
+
+   const correlationId = crypto.randomUUID();
+
+   const engineRequest = new Promise((resolve)=>{
+    pendingRequest.set(
+        correlationId,
+        resolve
+    )
+   })
+
+   await client.xAdd(
+    "engine:request",
+    "*",
+    {
+        correlationId,
+        responseTo:`response:${backendId}`,
+        requestType: "get-position",
+        userId: user.id,
+        symbol: symbol
+    }
+   )
+
+   const response = await engineRequest
+
+   return res.json({
+    response
+   })
+
+});
+engineRouter.get("/position", async (req,res)=>{
+    const user = req.user;
+    const correlationId = crypto.randomUUID()
+
+    const engineRequest = new Promise ((resolve)=>{
+        pendingRequest.set(
+            correlationId,
+            resolve
+        )
+    })
+
+    await client.xAdd(
+        "engine:request",
+        "*",
+        {
+            correlationId,
+            responseTo:`response:${backendId}`,
+            requestType: "get-user-position",
+            userId:user.id
+        }
+    )
+
+    const response = await engineRequest
+
+    return res.json({
+        response
+    })
+})
 engineRouter.get("/positions/closed/:marketId", (req, res) => {});
 engineRouter.get("/orders/open/:marketId", (req, res) => {})
 engineRouter.get("/orders/:marketId", (req, res) => {})
